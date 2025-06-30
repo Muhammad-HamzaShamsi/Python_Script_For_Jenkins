98516bf5cb27484852d68bbf96ee3c6754eb3273
Muhammad Hamza Shamsi <muhammadhamzashamsi35@gmail.com>
2025-06-30 17:28:01 +0200

    Add files via upload

---
+++ Python_Script.py
@@ -0,0 +1,13 @@
+# Basic Python Script
+
+# Ask for user input
+name = input("What is your name? ")
+age = int(input("How old are you? "))
+
+# Calculate the year the user will turn 100
+current_year = 2025
+year_turn_100 = current_year + (100 - age)
+
+# Print the result
+print(f"Hello, {name}!")
+print(f"You will turn 100 years old in the year {year_turn_100}.")
